f14ba2de47207b9780554ef41afe639f80ecfc7d
hoangphu1607 <62040675+hoangphu1607@users.noreply.github.com>
2021-04-18 15:42:38 +0700

    demo

---
+++ Code/Code/Caculator/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_1 = new System.Windows.Forms.Button();
+            this.txt_result = new System.Windows.Forms.TextBox();
+            this.bt_2 = new System.Windows.Forms.Button();
+            this.bt_3 = new System.Windows.Forms.Button();
+            this.bt_6 = new System.Windows.Forms.Button();
+            this.bt_5 = new System.Windows.Forms.Button();
+            this.bt_4 = new System.Windows.Forms.Button();
+            this.bt_9 = new System.Windows.Forms.Button();
+            this.bt_8 = new System.Windows.Forms.Button();
+            this.bt_7 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.bt_0 = new System.Windows.Forms.Button();
+            this.bt_add = new System.Windows.Forms.Button();
+            this.bt_div = new System.Windows.Forms.Button();
+            this.bt_mul = new System.Windows.Forms.Button();
+            this.bt_sub = new System.Windows.Forms.Button();
+            this.bt_result = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_1
+            // 
+            this.bt_1.Location = new System.Drawing.Point(32, 250);
+            this.bt_1.Name = "bt_1";
+            this.bt_1.Size = new System.Drawing.Size(40, 41);
+            this.bt_1.TabIndex = 0;
+            this.bt_1.Text = "1";
+            this.bt_1.UseVisualStyleBackColor = true;
+            this.bt_1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_result
+            // 
+            this.txt_result.Enabled = false;
+            this.txt_result.Location = new System.Drawing.Point(32, 42);
+            this.txt_result.Multiline = true;
+            this.txt_result.Name = "txt_result";
+            this.txt_result.Size = new System.Drawing.Size(194, 47);
+            this.txt_result.TabIndex = 1;
+            this.txt_result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // bt_2
+            // 
+            this.bt_2.Location = new System.Drawing.Point(84, 250);
+            this.bt_2.Name = "bt_2";
+            this.bt_2.Size = new System.Drawing.Size(40, 41);
+            this.bt_2.TabIndex = 2;
+            this.bt_2.Text = "2";
+            this.bt_2.UseVisualStyleBackColor = true;
+            this.bt_2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // bt_3
+            // 
+            this.bt_3.Location = new System.Drawing.Point(136, 250);
+            this.bt_3.Name = "bt_3";
+            this.bt_3.Size = new System.Drawing.Size(40, 41);
+            this.bt_3.TabIndex = 3;
+            this.bt_3.Text = "3";
+            this.bt_3.UseVisualStyleBackColor = true;
+            this.bt_3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // bt_6
+            // 
+            this.bt_6.Location = new System.Drawing.Point(136, 202);
+            this.bt_6.Name = "bt_6";
+            this.bt_6.Size = new System.Drawing.Size(40, 41);
+            this.bt_6.TabIndex = 6;
+            this.bt_6.Text = "6";
+            this.bt_6.UseVisualStyleBackColor = true;
+            this.bt_6.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // bt_5
+            // 
+            this.bt_5.Location = new System.Drawing.Point(84, 202);
+            this.bt_5.Name = "bt_5";
+            this.bt_5.Size = new System.Drawing.Size(40, 41);
+            this.bt_5.TabIndex = 5;
+            this.bt_5.Text = "5";
+            this.bt_5.UseVisualStyleBackColor = true;
+            this.bt_5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // bt_4
+            // 
+            this.bt_4.Location = new System.Drawing.Point(32, 202);
+            this.bt_4.Name = "bt_4";
+            this.bt_4.Size = new System.Drawing.Size(40, 41);
+            this.bt_4.TabIndex = 4;
+            this.bt_4.Text = "4";
+            this.bt_4.UseVisualStyleBackColor = true;
+            this.bt_4.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // bt_9
+            // 
+            this.bt_9.Location = new System.Drawing.Point(136, 155);
+            this.bt_9.Name = "bt_9";
+            this.bt_9.Size = new System.Drawing.Size(40, 41);
+            this.bt_9.TabIndex = 9;
+            this.bt_9.Text = "9";
+            this.bt_9.UseVisualStyleBackColor = true;
+            this.bt_9.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // bt_8
+            // 
+            this.bt_8.Location = new System.Drawing.Point(84, 155);
+            this.bt_8.Name = "bt_8";
+            this.bt_8.Size = new System.Drawing.Size(40, 41);
+            this.bt_8.TabIndex = 8;
+            this.bt_8.Text = "8";
+            this.bt_8.UseVisualStyleBackColor = true;
+            this.bt_8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // bt_7
+            // 
+            this.bt_7.Location = new System.Drawing.Point(32, 155);
+            this.bt_7.Name = "bt_7";
+            this.bt_7.Size = new System.Drawing.Size(40, 41);
+            this.bt_7.TabIndex = 7;
+            this.bt_7.Text = "7";
+            this.bt_7.UseVisualStyleBackColor = true;
+            this.bt_7.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(32, 108);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(40, 41);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "AC";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(84, 109);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(40, 41);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "C";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // bt_0
+            // 
+            this.bt_0.Location = new System.Drawing.Point(32, 297);
+            this.bt_0.Name = "bt_0";
+            this.bt_0.Size = new System.Drawing.Size(40, 41);
+            this.bt_0.TabIndex = 10;
+            this.bt_0.Text = "0";
+            this.bt_0.UseVisualStyleBackColor = true;
+            this.bt_0.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // bt_add
+            // 
+            this.bt_add.Location = new System.Drawing.Point(186, 109);
+            this.bt_add.Name = "bt_add";
+            this.bt_add.Size = new System.Drawing.Size(40, 41);
+            this.bt_add.TabIndex = 16;
+            this.bt_add.Text = "+";
+            this.bt_add.UseVisualStyleBackColor = true;
+            this.bt_add.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // bt_div
+            // 
+            this.bt_div.Location = new System.Drawing.Point(186, 250);
+            this.bt_div.Name = "bt_div";
+            this.bt_div.Size = new System.Drawing.Size(40, 41);
+            this.bt_div.TabIndex = 15;
+            this.bt_div.Text = "/";
+            this.bt_div.UseVisualStyleBackColor = true;
+            this.bt_div.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // bt_mul
+            // 
+            this.bt_mul.Location = new System.Drawing.Point(186, 202);
+            this.bt_mul.Name = "bt_mul";
+            this.bt_mul.Size = new System.Drawing.Size(40, 41);
+            this.bt_mul.TabIndex = 14;
+            this.bt_mul.Text = "*";
+            this.bt_mul.UseVisualStyleBackColor = true;
+            this.bt_mul.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // bt_sub
+            // 
+            this.bt_sub.Location = new System.Drawing.Point(186, 156);
+            this.bt_sub.Name = "bt_sub";
+            this.bt_sub.Size = new System.Drawing.Size(40, 41);
+            this.bt_sub.TabIndex = 13;
+            this.bt_sub.Text = "-";
+            this.bt_sub.UseVisualStyleBackColor = true;
+            this.bt_sub.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // bt_result
+            // 
+            this.bt_result.Location = new System.Drawing.Point(136, 296);
+            this.bt_result.Name = "bt_result";
+            this.bt_result.Size = new System.Drawing.Size(90, 41);
+            this.bt_result.TabIndex = 17;
+            this.bt_result.Text = "=";
+            this.bt_result.UseVisualStyleBackColor = true;
+            this.bt_result.Click += new System.EventHandler(this.button17_Click_1);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(84, 297);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(40, 41);
+            this.button18.TabIndex = 18;
+            this.button18.Text = ".";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(136, 109);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(40, 41);
+            this.button19.TabIndex = 19;
+            this.button19.Text = "<-";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(266, 359);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.bt_result);
+            this.Controls.Add(this.bt_add);
+            this.Controls.Add(this.bt_div);
+            this.Controls.Add(this.bt_mul);
+            this.Controls.Add(this.bt_sub);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.bt_0);
+            this.Controls.Add(this.bt_9);
+            this.Controls.Add(this.bt_8);
+            this.Controls.Add(this.bt_7);
+            this.Controls.Add(this.bt_6);
+            this.Controls.Add(this.bt_5);
+            this.Controls.Add(this.bt_4);
+            this.Controls.Add(this.bt_3);
+            this.Controls.Add(this.bt_2);
+            this.Controls.Add(this.txt_result);
+            this.Controls.Add(this.bt_1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_1;
+        private System.Windows.Forms.TextBox txt_result;
+        private System.Windows.Forms.Button bt_2;
+        private System.Windows.Forms.Button bt_3;
+        private System.Windows.Forms.Button bt_6;
+        private System.Windows.Forms.Button bt_5;
+        private System.Windows.Forms.Button bt_4;
+        private System.Windows.Forms.Button bt_9;
+        private System.Windows.Forms.Button bt_8;
+        private System.Windows.Forms.Button bt_7;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button bt_0;
+        private System.Windows.Forms.Button bt_add;
+        private System.Windows.Forms.Button bt_div;
+        private System.Windows.Forms.Button bt_mul;
+        private System.Windows.Forms.Button bt_sub;
+        private System.Windows.Forms.Button bt_result;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+    }
+}
+
---
+++ Code/Code/Caculator/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        double number1, number2, result;
+        //
+        bool trangthai = true;
+        //trangthai = true khi chua kich hoat toan hang (+ -)
+        String math = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                if(trangthai == false)
+                {
+                    txt_result.Text = "";
+                    trangthai = true;
+                }
+                String number = "1";
+                txt_result.Text += number;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (trangthai == false)
+            {
+                txt_result.Text = "";
+                trangthai = true;
+            }
+            String number = "2";
+                txt_result.Text += number;
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (trangthai == false)
+            {
+                txt_result.Text = "";
+                trangthai = true;
+            }
+            String number = "3";
+                txt_result.Text += number;
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (trangthai == false)
+            {
+                txt_result.Text = "";
+                trangthai = true;
+            }
+            String number = "5";
+                txt_result.Text += number;
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (trangthai == false)
+            {
+                txt_result.Text = "";
+                trangthai = true;
+            }
+            String number = "6";
+                txt_result.Text += number;
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (trangthai == false)
+            {
+                txt_result.Text = "";
+                trangthai = true;
+            }
+            String number = "7";
+                txt_result.Text += number;
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (trangthai == false)
+            {
+                txt_result.Text = "";
+                trangthai = true;
+            }
+            String number = "8";
+            txt_result.Text += number;
+
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (trangthai == false)
+            {
+                txt_result.Text = "";
+                trangthai = true;
+            }
+            String number = "9";
+                txt_result.Text += number;
+            
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if(trangthai == true)
+            {
+                math = "+";
+                txt_result.Text += " + ";
+                trangthai = false;
+            }
+            
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if(trangthai == true)
+            {
+                math = "-";
+                txt_result.Text += " - ";
+                trangthai = false;
+            }
+            
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if(trangthai == true)
+            {
+                math = "*";
+                txt_result.Text += " * ";
+                trangthai = false;
+
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            if(trangthai == true)
+            {
+                math = "/";
+                txt_result.Text += " / ";
+                trangthai = false;
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click_1(object sender, EventArgs e)
+        {
+            String[] kq = txt_result.Text.Split(' ');
+            try
+            {
+                switch (kq[1])
+                {
+                    case "+":
+                        number1 = Convert.ToDouble(kq[0]);
+                        number2 = Convert.ToDouble(kq[2]);
+                        result = number1 + number2;
+                        txt_result.Text = result.ToString();
+                        trangthai = false;
+                        break;
+                    case "-":
+                        number1 = Convert.ToDouble(kq[0]);
+                        number2 = Convert.ToDouble(kq[2]);
+                        result = number1 - number2;
+                        txt_result.Text = result.ToString();
+                        trangthai = false;
+                        break;
+                    case "*":
+                        number1 = Convert.ToDouble(kq[0]);
+                        number2 = Convert.ToDouble(kq[2]);
+                        result = number1 * number2;
+                        txt_result.Text = result.ToString();
+                        trangthai = false;
+                        break;
+                    case "/":
+                        number1 = Convert.ToDouble(kq[0]);
+                        number2 = Convert.ToDouble(kq[2]);
+                        if (number2 != 0)
+                        {
+                            result = number1 / number2;
+                            txt_result.Text = result.ToString();
+                            trangthai = false;
+                            break;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Khong The Chia 0");
+                            break;
+                        }
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Không Thể Tính");
+            }
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (trangthai == false)
+            {
+                txt_result.Text = "";
+                trangthai = true;
+            }
+            String number = "4";
+                txt_result.Text += number;
+            
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            if (trangthai == true)
+            {
+                String kq = txt_result.Text;
+                int lenght = txt_result.Text.Length;
+                kq = kq.Remove(lenght - 1);
+                txt_result.Text = kq;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            String[] kq = txt_result.Text.Split(' ');
+            int dodai = kq.Length;
+            switch (dodai)
+            {
+                case 1:
+                    txt_result.Text = "";
+                    break;
+                case 2:
+                    txt_result.Text = kq[0];
+                    break;
+                case 3:
+                    txt_result.Text = kq[0] + " " + kq[1] + " ";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            txt_result.Text = "";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (trangthai == false)
+            {
+                txt_result.Text = "";
+                trangthai = true;
+            }
+            String number = "0";
+                txt_result.Text += number;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
